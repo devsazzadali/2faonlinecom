ec2fcf6e26cc47d6666e6791997e4229acccda48
Lucky <luckyseifus@gmail.com>
2026-01-04 00:01:33 +0600

    first commit

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-2 w-2 bg-blue-500"></span>
         </span>
-        Client-Side Encryption Only
+        Saved Your Data In Your Local Storage
       </motion.div>
     </section>
   );
